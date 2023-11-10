42afa807a273eb526018e086ef5e390edd3d7572
Arthur Matias <ahmmfdc@gmail.com>
2023-11-10 07:12:59 +0000

    update image imports

--- src/Storage/SystemState.ts
+++ src/Storage/SystemState.ts
@@ -3,24 +3,37 @@ import MainColor from "../Components/Scalable/SettingsOptions/MainColor.svelte";
 import Theme from "../Components/Scalable/SettingsOptions/Theme.svelte";
 import Transparency from "../Components/Scalable/SettingsOptions/Transparency.svelte";
 import BackgroundSelector from "../Components/Scalable/SettingsOptions/BackgroundSelector.svelte";
-
+import img1 from '/wallpapers/1.png';
+import img2 from '/wallpapers/2.png';
+import img3 from '/wallpapers/3.png';
+import img4 from '/wallpapers/4.png';
+import img5 from '/wallpapers/5.png';
+import img6 from '/wallpapers/6.png';
+import img7 from '/wallpapers/7.png';
+import img8 from '/wallpapers/8.png';
+import img9 from '/wallpapers/9.png';
+import img10 from '/wallpapers/10.png';
+import img11 from '/wallpapers/11.png';
+import img12 from '/wallpapers/12.png';
+import img13 from '/wallpapers/13.png';
+import img14 from '/wallpapers/14.png';
 function SystemState() {
 
     const images = [
-        '/wallpapers/1.png',
-        '/wallpapers/2.png',
-        '/wallpapers/3.png',
-        '/wallpapers/4.png',
-        '/wallpapers/5.png',
-        '/wallpapers/6.png',
-        '/wallpapers/7.png',
-        '/wallpapers/8.png',
-        '/wallpapers/9.png',
-        '/wallpapers/10.png',
-        '/wallpapers/11.png',
-        '/wallpapers/12.png',
-        '/wallpapers/13.png',
-        '/wallpapers/14.png'
+        img1,
+        img2,
+        img3,
+        img4,
+        img5,
+        img6,
+        img7,
+        img8,
+        img9,
+        img10,
+        img11,
+        img12,
+        img13,
+        img14
     ]
     const pt: SystemTexts = {
         config: {
